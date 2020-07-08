7519dcff397c8415ad54473384017cead7451b82
Keisuke <atcq2003@hotmail.com>
2020-07-09 05:29:10 +0900

    CoreXlsxのインストールと.git ignore作成

--- excel/ViewController.swift
+++ excel/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreXLSX
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let filepath = "./categories.xlsx"
+        guard let file = XLSXFile(filepath: filepath) else {
+            fatalError("XLSX file at \(filepath) is corrupted or does not exist")
+        }
+        
+        for wbk in try file.parseWorkbooks() {
+            for (name, path) in try file.parseWorksheetPathsAndNames(workbook: wbk) {
+                if let worksheetName = name {
+                    print("This worksheet has a name: \(worksheetName)")
+                }
+                
+                let worksheet = try file.parseWorksheet(at: path)
+                for row in worksheet.data?.rows ?? [] {
+                    for c in row.cells {
+                        print(c)
+                    }
+                }
+            }
+        }
+        
     }
-
-
+    
 }
 
